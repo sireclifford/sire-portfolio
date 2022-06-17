f856311f71b21f1f8c7a635d91a1bcaffbce2275
sireclifford <dev.sire.clifford@gmail.com>
2022-06-17 14:44:45 +0000

    fixed linters

--- scripts/form.js
+++ scripts/form.js
@@ -1,12 +1,11 @@
 const emailInput = document.querySelector('#user-email');
 const form = document.querySelector('#signup');
 
-
 const destopForm = document.querySelector('#desktopSignup');
 const userEmail = document.querySelector('#useremail');
 
 const showErrorMsg = (type) => {
-  const error = type==='mobile' ? document.querySelectorAll('small')[0] : document.querySelectorAll('small')[1];
+  const error = type === 'mobile' ? document.querySelectorAll('small')[0] : document.querySelectorAll('small')[1];
   error.style.color = 'red';
   error.marginTop = '8px';
   error.textContent = 'email address should be in lowercase';
@@ -19,9 +18,7 @@ const showSuccessMsg = () => {
   sucess.textContent = 'Thank you!';
 };
 
-
 destopForm.addEventListener('submit', (e) => {
-  
   const email = userEmail.value.trim();
   const convertedEmail = email.toUpperCase();
   if (email === convertedEmail) {
@@ -33,7 +30,6 @@ destopForm.addEventListener('submit', (e) => {
 });
 
 form.addEventListener('submit', (e) => {
-
   const email = emailInput.value.trim();
   const convertedEmail = email.toUpperCase();
   if (email === convertedEmail) {
