c85d7a75e37d3615c7f3f639528e030546e7faf1
sireclifford <dev.sire.clifford@gmail.com>
2022-06-17 05:22:52 +0000

    Add popups for mobile and desktop using Js to feed data from Js objects

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,89 @@
+const portfolioData = [
+  {
+    id: 1,
+    title: 'Uber',
+    skills: [
+      'Ruby on Rails',
+      'Css',
+      'Javascript',
+      'React',
+      'Redux',
+      'NodeJS',
+      'Express',
+      'MongoDB',
+      'PostgreSQL',
+    ],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: './assets/images/uber.jpeg',
+    source: 'https://github.com/sireclifford/sire-portfolio',
+  },
+  {
+    id: 2,
+    title: 'Duolingo',
+    skills: [
+      'React',
+      'NodeJS',
+      'Css',
+      'PostgreSQL',
+    ],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: './assets/images/duolingo.webp',
+    source: 'https://github.com/sireclifford/sire-portfolio',
+  },
+  {
+    id: 3,
+    title: 'Airbnb',
+    skills: [
+      'React',
+      'Ruby on Rails',
+      'Css',
+      'React',
+      'PostgreSQL',
+    ],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: './assets/images/airbnb.png',
+    source: 'https://github.com/sireclifford/sire-portfolio',
+  },
+  {
+    id: 4,
+    title: 'TikTok',
+    skills: [
+      'React',
+      'Ruby on Rails',
+      'Css',
+      'React',
+      'PostgreSQL',
+    ],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: './assets/images/tiktok.webp',
+    source: 'https://github.com/sireclifford/sire-portfolio',
+  },
+  {
+    id: 5,
+    title: 'Deliveroo',
+    skills: [
+      'React',
+      'Ruby on Rails',
+      'Css',
+      'React',
+      'PostgreSQL',
+    ],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: './assets/images/deliveroo.svg',
+    source: 'https://github.com/sireclifford/sire-portfolio',
+  },
+
+];
 
 const loadRecentWork = () => {
   // define media queries
-  const mobileMediaQuery = window.matchMedia('(max-width: 600px)')
+  const mobileMediaQuery = window.matchMedia('(max-width: 600px)');
 
   const work = document.querySelector('.recent-work-list');
-  fetch('./data/recent-works.json')
-    .then((response) => response.json())
-    .then((data) => {
-      const projects = data['RecentWorks'].map((project) =>
-        `<div class="work-card">
+
+  const desktopClose = document.getElementsByClassName('desktop-close-popup')[0];
+
+  const projects = portfolioData.map((project) => `<div class="work-card">
       <div class="card-header">
         <img src="${project.image}" alt="${project.title}"/>
       </div>
@@ -17,117 +92,98 @@ const loadRecentWork = () => {
         <ul>
         ${project.skills.map((item) => `<li>${item}</li>`).join('')}
         </ul>
-        <a href="#" class="btn-success btn-three ${mobileMediaQuery.matches ? 'single-project-btn' : 'desktp-view-project-btn'}"
+        <a id="${project.id}" href="#" class="btn-success btn-three ${mobileMediaQuery.matches ? 'single-project-btn' : 'desktp-view-project-btn'}"
           >See Project</a
           single-project-btns
         >
       </div>
     </div>`).join('');
-      // create a virtual container
-      const range = document.createRange();
-      // give it a context
-      range.selectNode(work);
-      // add the html, this converts the html into a collection of elements
-      const fragment = range.createContextualFragment(projects);
-      // append the elements to the document
-      work.appendChild(fragment);
+  // create a virtual container
+  const range = document.createRange();
+  // give it a context
+  range.selectNode(work);
+  // add the html, this converts the html into a collection of elements
+  const fragment = range.createContextualFragment(projects);
+  // append the elements to the document
+  work.appendChild(fragment);
 
+  // add event listener to each project
+  const singleProjectModal = document.querySelector('#single-project-modal');
+  const singleProjectBtn = document.querySelectorAll('.single-project-btn');
+  singleProjectBtn.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      const foundProject = portfolioData.filter((x) => String(x.id) === event.target.id);
+      document.getElementsByClassName('single-project-pic')[0].src = foundProject[0].image;
+      document.getElementsByClassName('single-project-pic')[0].alt = foundProject[0].title;
+      document.getElementsByClassName('single-project-title')[0].innerHTML = foundProject[0].title;
+      document.getElementsByClassName('single-project-skills')[0].innerHTML = foundProject[0].skills.map((item) => `<li>${item}</li>`).join('');
+      document.getElementsByClassName('single-project-desc')[0].innerHTML = foundProject[0].description;
+      document.getElementsByClassName('project-source')[0].href = foundProject[0].source;
+      singleProjectModal.style.display = 'block';
 
-
-      // add event listener to each project
-      const singleProjectModal = document.querySelector('#single-project-modal');
-      const singleProjectBtn = document.querySelectorAll('.single-project-btn');
-      singleProjectBtn.forEach((btn) => {
-        btn.addEventListener('click', () => {
-          singleProjectModal.style.display = 'block';
-        }, true)
-      });
-
-
-      const desktopModal = document.querySelector('.desktop-modal');
-      // const body = document.querySelector('body');
-      const desktopProjectBtn = document.querySelectorAll('.desktp-view-project-btn');
-      desktopProjectBtn.forEach((btn) => {
-        btn.addEventListener('click', () => {
-          // body.style.filter = 'blur(8px)';
-          desktopModal.style.display = 'flex';
-
-        }, true)
-      })
-
-
-
+      const close = document.getElementsByClassName('close-popup')[0];
       close.onclick = () => {
         singleProjectModal.style.display = 'none';
+        range.detach();
       };
+    }, true);
+  });
 
-      desktopClose.onclick = () => {
-        desktopModal.style.display = 'none';
-      }
+  const desktopModal = document.querySelector('.desktop-modal');
+  // const body = document.querySelector('body');
+  const desktopProjectBtn = document.querySelectorAll('.desktp-view-project-btn');
+  desktopProjectBtn.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      // body.style.filter = 'blur(8px)';
+      const foundProject = portfolioData.filter((x) => String(x.id) === event.target.id);
+      document.getElementsByClassName('desktop-single-project-pic')[0].src = foundProject[0].image;
+      document.getElementsByClassName('desktop-single-project-pic')[0].alt = foundProject[0].title;
+      document.getElementsByClassName('desktop-single-project-title')[1].innerHTML = foundProject[0].title;
+      document.getElementsByClassName('desktop-single-project-skills')[0].innerHTML = foundProject[0].skills.map((item) => `<li>${item}</li>`).join('');
+      document.getElementsByClassName('desktop-single-project-desc')[0].innerHTML = foundProject[0].description;
+      document.getElementsByClassName('project-source')[0].href = foundProject[0].source;
+      desktopModal.style.display = 'flex';
+    }, true);
+  });
 
-
-    });
+  desktopClose.onclick = () => {
+    desktopModal.style.display = 'none';
+  };
 };
 
 loadRecentWork();
 
-
-
-
-
-
-
-
-const modal = document.querySelector('#myModal');
-const btn = document.querySelector('#hamburger-icon');
-const span = document.getElementsByClassName('close')[0];
-const close = document.getElementsByClassName('close-popup')[0];
-const desktopClose = document.getElementsByClassName('desktop-close-popup')[0];
-
-btn.onclick = () => {
-  modal.style.display = 'block';
-};
-span.onclick = () => {
-  modal.style.display = 'none';
-};
-
-// window.onclick = (event) => {
-//   if (event.target === modal) {
-//     modal.style.display = 'none';
-//   }
-// };
-
-
-// window.onclick = (event) => {
-//   if (event.target === modal) {
-//     singleProjectModal.style.display = 'none';
-//   }
-// };
-
-const GotoProjects = () => {
+const loadActions = () => {
+  const modal = document.querySelector('#myModal');
+  const btn = document.querySelector('#hamburger-icon');
+  const span = document.getElementsByClassName('close')[0];
   const projectsLink = document.querySelector('#projects-link');
+  const gettingStartedLink = document.querySelector('#getting-started-link');
+  const aboutLink = document.querySelector('#about-link');
+
+  btn.addEventListener('click', () => {
+    modal.style.display = 'block';
+  },
+  true);
+
+  span.addEventListener('click', () => {
+    modal.style.display = 'none';
+  },
+  true);
+
   projectsLink.addEventListener('click', () => {
     modal.style.display = 'none';
     document.getElementById('recent-works').scrollIntoView();
   });
-};
-const GotoGettingStarted = () => {
-  const gettingStartedLink = document.querySelector('#getting-started-link');
+
   gettingStartedLink.addEventListener('click', () => {
     modal.style.display = 'none';
     document.getElementById('mobile-contact-me').scrollIntoView();
   });
-};
-const GotoAbout = () => {
-  const aboutLink = document.querySelector('#about-link');
+
   aboutLink.addEventListener('click', () => {
     modal.style.display = 'none';
     document.getElementById('about-me').scrollIntoView();
   });
 };
-
-
-
-GotoProjects();
-GotoGettingStarted();
-GotoAbout();
+loadActions();
